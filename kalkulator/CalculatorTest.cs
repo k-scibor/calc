88ae9583a2ab2f5d51604bd0a156b35d66991b70
k-scibor <uT_3780@zsp3zamosc.pl>
2025-04-24 09:31:33 +0200

    f

---
+++ kalkulator/CalculatorTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+[TestClass]
+
+public class CalculatorTest
+
+{
+
+    [TestMethod]
+
+    public void Add_ReturnsCorrectResult()
+
+    {
+
+        var calculator = new Calculator();
+
+        double result = calculator.Add(2, 3);
+
+        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(5, result);
+
+    }
+
+
+
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+
+    public void Divide_ByZero_ThrowsException()
+
+    {
+
+        var calculator = new Calculator();
+
+        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.ThrowsException<DivideByZeroException>(() => calculator.Divide(5, 0));
+
+    }
+
+} 
+
---
+++ kalkulator/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+
+{
+
+    static void Main(string[] args)
+
+    {
+
+        var calculator = new Calculator();
+
+
+
+        double result = calculator.Add(2, 3);
+
+        Console.WriteLine($"2 + 3 = {result}");
+
+
+
+        try
+
+        {
+
+            calculator.Divide(5, 0);
+
+        }
+
+        catch (DivideByZeroException)
+
+        {
+
+            Console.WriteLine("Błąd: dzielenie przez zero.");
+
+        }
+
+
+
+        Console.WriteLine("Naciśnij dowolny klawisz, aby zakończyć...");
+
+        Console.ReadKey();
+
+    }
+
+} 
+
